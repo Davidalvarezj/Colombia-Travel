8400075d30f627c75d3ddb47ced37ced099a60eb
David Alvarez-mac <davidalvarezj@gmail.com>
2022-10-26 14:41:31 -0500

    Main page working carousel ok

--- run.js
+++ run.js
@@ -1,5 +1,11 @@
-var myCarousel = document.getElementById('myCarousel')
 
-myCarousel.addEventListener('slide.bs.carousel', function () {
-  // do something...
-})
+const navbar = document.querySelector('.navbar');
+const val = document.querySelector('.val');
+window.onscroll = () => {
+    if (window.scrollY > 1500) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+    val.innerHTML=window.scrollY
+};
