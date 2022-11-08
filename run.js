36f702f9d56e75e99816e73c240a94246b781804
David Alvarez-mac <davidalvarezj@gmail.com>
2022-11-07 20:23:54 -0500

    Added modals to shoping butoons

--- run.js
+++ run.js
@@ -1,11 +1,13 @@
 
 const navbar = document.querySelector('.navbar');
-const val = document.querySelector('.val');
+const dropdown = document.querySelector('.dropdown-menu');
 window.onscroll = () => {
-    if (window.scrollY > 1500) {
+    if (window.scrollY > 170) {
         navbar.classList.add('nav-active');
+        dropdown.classList.add('dropdown-dark')
     } else {
         navbar.classList.remove('nav-active');
+        dropdown.classList.remove('dropdown-dark');
     }
     val.innerHTML=window.scrollY
 };
